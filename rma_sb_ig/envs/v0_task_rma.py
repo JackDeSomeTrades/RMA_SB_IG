c0fe71b023b2bb04f41f378e14bdb3a5dac7b7ae
Vasishta Pavan <pavan.vasishta@ensta-paris.fr>
2022-04-13 14:11:38 +0200

    Working on making v0 work with RMA

--- rma_sb_ig/envs/v0_task_rma.py
+++ rma_sb_ig/envs/v0_task_rma.py
@@ -144,7 +144,7 @@ class V0LeggedRobotTask(EnvScene, BaseTask):
                         feet_contact_switches - 4
                         previous_actions -12
                         Mass - 1
-                        Gravity - 1
+                        Gravity (projected) - 3
                         COM_x - 1
                         COM_y - 1
                         COM_z - 1
@@ -159,8 +159,8 @@ class V0LeggedRobotTask(EnvScene, BaseTask):
             [-math.inf] +
             [0] * 4 +
             self.lower_bounds_joints +
-            [0] +            # Mass
-            [-math.inf] +    # Gravity
+            [0] +             # Mass
+            [-math.inf] * 3 + # Projected Gravity
             [-math.inf] +
             [-math.inf] +
             [-math.inf] +
@@ -175,7 +175,7 @@ class V0LeggedRobotTask(EnvScene, BaseTask):
             [1] * 4 +
             self.upper_bounds_joints +
             [math.inf] +      # Mass
-            [0] +             # Gravity
+            [0] * 3 +         # Projected Gravity
             [math.inf] +
             [math.inf] +
             [math.inf] +
@@ -244,8 +244,8 @@ class V0LeggedRobotTask(EnvScene, BaseTask):
         noise_vec[24:26] = noise_scales.ang_vel * noise_level * self.obs_scales.ang_vel
         noise_vec[26:30] = 0.
         noise_vec[30:42] = 0.  # Previous action.
-        noise_vec[42:48] = 0.  # Mass, Gravity and COM
-        noise_vec[48:52] = noise_scales.height_measurements * noise_level * self.obs_scales.height_measurements
+        noise_vec[42:50] = 0.  # Mass, Gravity and COM
+        noise_vec[50:54] = noise_scales.height_measurements * noise_level * self.obs_scales.height_measurements
 
         return noise_vec
 
@@ -423,7 +423,7 @@ class V0LeggedRobotTask(EnvScene, BaseTask):
             self.body_com_y.unsqueeze(-1),
             self.body_com_z.unsqueeze(-1),
             self.friction_coeffs.squeeze(-1),
-            local_terrain_height.unsqueeze(-1)
+            local_terrain_height
         ), dim=-1)
 
         self.obs_buf = torch.cat((self.X_t,
@@ -632,6 +632,165 @@ class V0LeggedRobotTask(EnvScene, BaseTask):
 
     def get_privileged_observations(self):
         return self.privileged_obs_buf
+
+        # -------------- Reward functions begin below: --------------------------------#
+
+    def _reward_forward(self):
+        base_x_velocity = self.base_lin_vel[:, 0]
+        MAX_FWD_VEL = 1.0
+        forward = quat_apply(self.base_quat, self.forward_vec)
+        # max_fwd_vel_tensor = torch.full_like(base_x_velocity, MAX_FWD_VEL)
+        # reward = torch.abs(base_x_velocity - max_fwd_vel_tensor)
+        diff = base_x_velocity - forward[:, 0]
+        reward = torch.linalg.norm(diff.unsqueeze(-1), dim=1, ord=1)  # L1 norm
+
+        return reward
+
+    def _reward_maintain_forward(self):
+        # Rewards forward motion at limited values (v_x)
+        base_x_velocity = self.base_lin_vel[:, 0]
+        MAX_FWD_VEL = 1.0
+        max_fwd_vel_tensor = torch.full_like(base_x_velocity, MAX_FWD_VEL)
+        reward = torch.fmin(base_x_velocity, max_fwd_vel_tensor)
+        # print(" forward x vel", base_x_velocity)
+        # print(" forward reward", reward)
+        reward[reward < 0.0] = 0.0  # TODO: Check if this is right.
+        # print(f"forward reward * {self.cfg.rewards.scales.forward}", reward)
+        return reward
+
+    def _reward_lateral_movement_rotation(self):
+        # penalises lateral motion (v_y) and limiting angular velocity yaw
+        reward = self.base_lin_vel[:, 1].pow(2).unsqueeze(-1).sum(dim=1) + self.base_ang_vel[:, 2].pow(2).unsqueeze(
+            -1).sum(dim=1)  # TODO check with 1
+        # print(f"lateral reward * {self.cfg.rewards.scales.lateral_movement_rotation}", reward)
+        return reward
+
+    def _reward_orientation(self):
+        # orientation = self.projected_gravity[:, :2]
+        orientation = torch.stack([self.base_rpy[0], self.base_rpy[1]], dim=1)
+        reward = torch.sum(torch.square(orientation), dim=1)
+        return reward
+
+    def _reward_work(self):
+        diff_joint_pos = self.actions - self.last_actions
+        # torque_transpose = torch.transpose(self.torques, 0, 1)
+        reward = torch.abs(torch.sum(torch.inner(self.torques, diff_joint_pos), dim=1))  # this is the L1 norm
+        return reward
+
+    def _reward_ground_impact(self):
+        reward = torch.sum(torch.square(self.contact_forces[:, self.feet_indices, 2] -
+                                        self.last_contact_forces[:, self.feet_indices, 2]),
+                           dim=1)  # only taking into account the vertical reaction, might need to check if parallel ground reaction makes sense.
+        return reward
+
+    def _reward_smoothness(self):
+        reward = torch.sum(torch.square(self.torques - self.last_torques), dim=1)
+        return reward
+
+    def _reward_action_magnitude(self):
+        reward = torch.sum(torch.square(self.actions), dim=1)
+        return reward
+
+    def _reward_joint_speed(self):
+        reward = torch.sum(torch.square(self.last_dof_vel), dim=1)
+        return reward
+
+    def _reward_z_acceleration(self):
+        reward = torch.square(self.base_lin_vel[:, 2])
+        return reward
+
+    def _reward_foot_slip(self):
+        reward = 0  # TODO: Fix this
+        return reward
+
+    def _reward_lin_vel_z(self):
+        # Penalize z axis base linear velocity
+        reward = torch.square(self.base_lin_vel[:, 2])
+        return reward
+
+    def _reward_ang_vel_xy(self):
+        # Penalize xy axes base angular velocity
+        return torch.sum(torch.square(self.base_ang_vel[:, :2]), dim=1)
+
+    def _reward_base_height(self):
+        # Make sure the robot body maintains a minimum distance from the ground based on the z center of mass.
+        base_height = torch.mean(self.root_states[:, 2].unsqueeze(1) - self.measured_heights, dim=1)
+        return torch.square(base_height - self.cfg.rewards.base_height_target)
+
+    def _reward_torques(self):
+        # Penalize torques
+        return torch.sum(torch.square(self.torques), dim=1)
+
+    def _reward_dof_vel(self):
+        # Penalize dof velocities
+        return torch.sum(torch.square(self.dof_vel), dim=1)
+
+    def _reward_dof_acc(self):
+        # Penalize dof accelerations
+        return torch.sum(torch.square((self.last_dof_vel - self.dof_vel) / self.dt), dim=1)
+
+    def _reward_action_rate(self):
+        # Penalize changes in actions
+        return torch.sum(torch.square(self.last_actions - self.actions), dim=1)
+
+    def _reward_collision(self):
+        # Penalize collisions on selected bodies
+        return torch.sum(1. * (torch.norm(self.contact_forces[:, self.penalised_contact_indices, :], dim=-1) > 0.1),
+                         dim=1)
+
+    def _reward_termination(self):
+        # Terminal reward / penalty
+        return self.reset_buf * ~self.time_out_buf
+
+    def _reward_dof_pos_limits(self):
+        # Penalize dof positions too close to the limit
+        out_of_limits = -(self.dof_pos - self.dof_pos_limits[:, 0]).clip(max=0.)  # lower limit
+        out_of_limits += (self.dof_pos - self.dof_pos_limits[:, 1]).clip(min=0.)
+        return torch.sum(out_of_limits, dim=1)
+
+    def _reward_dof_vel_limits(self):
+        # Penalize dof velocities too close to the limit
+        # clip to max error = 1 rad/s per joint to avoid huge penalties
+        return torch.sum(
+            (torch.abs(self.dof_vel) - self.dof_vel_limits * self.cfg.rewards.soft_dof_vel_limit).clip(min=0., max=1.),
+            dim=1)
+
+    def _reward_tracking_lin_vel(self):
+        # Tracking of linear velocity commands (xy axes)
+        lin_vel_error = torch.sum(torch.square(self.commands[:, :2] - self.base_lin_vel[:, :2]), dim=1)
+        reward = torch.exp(-lin_vel_error / self.cfg.rewards.tracking_sigma)
+        return reward
+
+    def _reward_tracking_ang_vel(self):
+        # Tracking of angular velocity commands (yaw)
+        ang_vel_error = torch.square(self.commands[:, 2] - self.base_ang_vel[:, 2])
+        return torch.exp(-ang_vel_error / self.cfg.rewards.tracking_sigma)
+
+    def _reward_feet_air_time(self):
+        # Reward long steps
+        # Need to filter the contacts because the contact reporting of PhysX is unreliable on meshes
+        contact = self.contact_forces[:, self.feet_indices, 2] > 1.
+        contact_filt = torch.logical_or(contact, self.last_contacts)
+        self.last_contacts = contact
+        first_contact = (self.feet_air_time > 0.) * contact_filt
+        self.feet_air_time += self.dt
+        rew_airTime = torch.sum((self.feet_air_time - 0.5) * first_contact,
+                                dim=1)  # reward only on first contact with the ground
+        rew_airTime *= torch.norm(self.commands[:, :2], dim=1) > 0.1  # no reward for zero command
+        self.feet_air_time *= ~contact_filt
+        return rew_airTime
+
+    def _reward_feet_stumble(self):
+        # Penalize feet hitting vertical surfaces
+        return torch.any(torch.norm(self.contact_forces[:, self.feet_indices, :2], dim=2) > \
+                         5 * torch.abs(self.contact_forces[:, self.feet_indices, 2]), dim=1)
+
+    def _reward_stand_still(self):
+        # Penalize motion at zero commands
+        return torch.sum(torch.abs(self.dof_pos - self.default_dof_pos), dim=1) * (
+                torch.norm(self.commands[:, :2], dim=1) < 0.1)
+
+    # ---------------Reward functions end here ---------------------- #
 
 
 
