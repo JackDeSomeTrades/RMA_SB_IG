2db9120bfa7c08c873f954d85fb6a84c4c8cc8fe
CesarPierr <cesar.pierre38@gmail.com>
2022-05-24 18:13:45 +0200

    add changes

--- rma_sb_ig/envs/soto_forward_task.py
+++ rma_sb_ig/envs/soto_forward_task.py
@@ -40,42 +40,47 @@ class SotoForwardTask(SotoEnvScene, BaseTask):
             self._process_test()
         else :
             self.gym.destroy_viewer(self.viewer)
+            self.gym.destroy_sim(self.sim)
 
     def _process_test(self):
-        # self.gym.prepare_sim(self.sim) #TODO : ATTENTION FAIT TOUT PLANTER
-        x = 0
+        self.gym.prepare_sim(self.sim) #TODO : ATTENTION FAIT TOUT PLANTER avec set actor dof state
+        #when gym.prepared_sim is called, have to work withroot state tensors : gym.set_actor_root_state_tensor(sim, _root_tensor)
+        # acquire root state tensor descriptor
+        step = 0
         while not self.gym.query_viewer_has_closed(self.viewer):
-            x += 1
+            step += 1
             # update viewer
             # set initial dof states
-            k = np.abs(np.sin(x / 100))
-            self.soto_current = k * self.soto_upper_limits + \
-                                (1 - k) * self.soto_lower_limits
-            self.default_dof_pos = self.soto_current
-
-            # remember : important pieces to control are conveyor belt left base link/conveyor belt right base link
-            self.default_dof_state["pos"] = self.default_dof_pos
-
-            # send to torch
-            self.default_dof_pos_tensor = to_torch(
-                self.default_dof_pos, device=self.device)
-
-            # set initial position targets
-            for env in self.envs :
-                # set dof states
-                self.gym.set_actor_dof_states(
-                    env, self.soto_handle, self.default_dof_state, gymapi.STATE_ALL)
-
-                # set position targets
-                self.gym.set_actor_dof_position_targets(
-                    env, self.soto_handle, self.default_dof_pos)
+            # set initial position targets #TODO : work only if sim is not prepared
+            # for env in self.envs :
+            #     # set dof states
+            #     self.gym.set_actor_dof_states(
+            #         env, self.soto_handle, self.default_dof_state, gymapi.STATE_ALL)
+            #
+            #     # set position targets
+            #     self.gym.set_actor_dof_position_targets(
+            #         env, self.soto_handle, self.default_dof_pos)
+            # k = np.abs(np.sin(step / 100))
+            # self.soto_current = k * self.soto_upper_limits + \
+            #                     (1 - k) * self.soto_lower_limits
+            # self.default_dof_pos = self.soto_current
+            #
+            # # remember : important pieces to control are conveyor belt left base link/conveyor belt right base link
+            # self.default_dof_state["pos"] = self.default_dof_pos
+            #
+            # # send to torch
+            # self.default_dof_pos_tensor = to_torch(
+            #     self.default_dof_pos, device=self.device)
 
             self.gym.step_graphics(self.sim)
             self.gym.draw_viewer(self.viewer, self.sim, False)
-            self.gym.sync_frame_time(self.sim)
+            #self.gym.sync_frame_time(self.sim) #synchronise simulation with real time
+            self.gym.simulate(self.sim)
+            self.gym.fetch_results(self.sim, True)
 
         # cleanup
         self.gym.destroy_viewer(self.viewer)
+        self.gym.destroy_sim(self.sim)
 
 
 
