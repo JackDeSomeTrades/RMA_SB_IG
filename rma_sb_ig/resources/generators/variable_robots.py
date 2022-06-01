362dfd6024fa1dbf26891c7513aca74dcc00566d
Vasishta Pavan <pavan.vasishta@ensta-paris.fr>
2022-06-01 15:41:19 +0200

    Changes to variable robot folder structure

--- rma_sb_ig/resources/generators/variable_robots.py
+++ rma_sb_ig/resources/generators/variable_robots.py
@@ -5,14 +5,14 @@ import lxml.etree as ET
 from pathlib import Path
 import subprocess
 import random
-
+from pathlib import Path
 
 
 NUMBER_OF_LEGS = [3, 4, 5, 6, 7]
 
 
 class VariableRobotDescriptor:
-    def __init__(self, fpath: str, config_dict: dict):
+    def __init__(self, xacro_template_fpath:str, xacro_gen_folderpath:str, gen_urdf_folderpath:str, config_dict:dict):
         """
         :param config_dict: dict
         Contains the following attributes:
@@ -21,15 +21,18 @@ class VariableRobotDescriptor:
         "modifyNbLegs": boolean
 
 
-        :param fpath: str
+        :param xacro_template_fpath: str
         Contains the file path for the xacro template to be modified.
         """
         self.conf_dict = config_dict
         self.xacrons = "http://www.ros.org/wiki/xacro"
 
-        self.fpath_xacro = fpath
+        self.fpath_xacro_template = xacro_template_fpath
+        self.fpath_xacro_gen = Path(xacro_gen_folderpath)
+        self.fpath_urdf_gen = Path(gen_urdf_folderpath)
+
         parser = ET.XMLParser(remove_blank_text=True)
-        self.tree = ET.parse(self.fpath_xacro, parser=parser)
+        self.tree = ET.parse(self.fpath_xacro_template, parser=parser)
         self.root = self.tree.getroot()
         self.ns = {'xacro': self.xacrons}
         self._get_body_size()
@@ -57,33 +60,44 @@ class VariableRobotDescriptor:
             z_pos = 0.75
 
             self.legspos.append((x_pos, y_pos, z_pos))
-        print("doen")
+        print("done")
 
-    def write_xacro(self, fpath:str):
+    def write_xacro(self, variant_name):
         # make xml elements for xacro:leg
         for val, legpos in enumerate(self.legspos):
             reflect_val = random.choice([-1, 1])
             leg_template = ET.Element('{'+f'{self.xacrons}'+'}'+'leg', attrib={'name': f'l{val+1}', 'reflect': f'{reflect_val}'}, nsmap=self.ns)
             leg_desc = ET.SubElement(leg_template, 'origin', attrib={'xyz': self._stringify_value(legpos), 'rpy': '0 0 0'})
-            # ET.dump(leg_template)
-
             self.root.append(leg_template)
 
-        if fpath is not None:
-            fl = open(fpath, 'wb')
+        if self.fpath_xacro_gen is not None:
+            self.fpath_xacro_gen = self.fpath_xacro_gen.joinpath(variant_name).with_suffix('.xacro')
+            fl = open(self.fpath_xacro_gen, 'wb')
             # self.tree.write(fl, xml_declaration=True, pretty_print=True)
             fl.write(ET.tostring(self.root, xml_declaration=True, pretty_print=True, with_tail=False))
             fl.close()
+            print("Wrote generated Xacro")
     
-    def make_urdf(self, outpath):
-        cmd = ["rosrun", "xacro", "xacro", f"{self.fpath_xacro} > {outpath}"]
-        # subprocess.run(cmd, env=os.environ, stdout=subprocess.PIPE, check=True)
-        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, env=os.environ, preexec_fn=os.setsid())
+    def make_urdf(self, variant_name):
+        self.fpath_urdf_gen = self.fpath_urdf_gen.joinpath(variant_name).with_suffix('.urdf')
+        # env = os.environ
+        # env["PATH"] += os.pathsep + '/opt/ros/melodic/bin'     # for the various package not found errors due to path issues
+        # env["LD_LIBRARY_PATH"] = '/opt/ros/melodic/lib'
+        # env["CMAKE_PREFIX_PATH"] = '/opt/ros/melodic'
+        subprocess.run(f'rosrun xacro xacro {self.fpath_xacro_gen}>{self.fpath_urdf_gen}', shell=True)
+
         print("Finished generating urdf")
+
+    def create(self, variant_name):
+        self.generate_legs()
+        self.write_xacro(variant_name)
+        self.make_urdf(variant_name)
 
 
 if __name__ == '__main__':
-    xacro_template = '/home/pavan/Workspace/RMA_SB_IG/rma_sb_ig/resources/robots/v0variants/urdf/v0_template.urdf.xacro'
+    xacro_template = '/home/pavan/Workspace/RMA_SB_IG/rma_sb_ig/resources/robots/v0variants/v0_template.urdf.xacro'
+    xacrogen_folder = '../robots/v0variants/xacros/'
+    urdfgen_folder = '../robots/v0variants/urdf/'
 
     configDict = {
         'robotDef': {'num_legs': 4},
@@ -91,7 +105,11 @@ if __name__ == '__main__':
         'modifyNbLegs': False
     }
 
-    variance = VariableRobotDescriptor(xacro_template, config_dict=configDict)
-    variance.generate_legs()
-    variance.write_xacro('/home/pavan/Workspace/RMA_SB_IG/rma_sb_ig/resources/robots/v0variants/urdf/v0_4_legs_generated.urdf.xacro')
-    variance.make_urdf('/home/pavan/Workspace/RMA_SB_IG/rma_sb_ig/resources/robots/v0variants/urdf/v0_4_legs_generated.urdf')
+    NB_GENERATED = 20
+
+    for i in range(NB_GENERATED):
+        gen_flname = f'v0_{configDict["robotDef"]["num_legs"]}l_{i}'
+        print(f"Generating variant:{i}")
+        variance = VariableRobotDescriptor(xacro_template, xacrogen_folder, urdfgen_folder, config_dict=configDict)
+        variance.create(gen_flname)
+
